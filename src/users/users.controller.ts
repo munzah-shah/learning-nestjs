1affedbf035e38de4f3c8ccffe86d6b6e10d57b8
munzah-shah <munzah.shah00@gmail.com>
2025-07-10 23:01:17 +0500

    Added all required routes in the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users route
+export class UsersController {
+    /* we'll have the following routes in our users API
+        GET: /users or /users?role=value (Query Paramaters)
+        GET: /users/:id
+        POST: /users
+        PATCH: /users/:id
+        DELETE /users/:id
+    */
+
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return []
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string ) { 
+        return { id }
+    }
+
+    @Post() //POST /users
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() updatedUser: {} ) { 
+        return { id, ...updatedUser }
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    delete(@Param('id') id: string) {
+        return { id }
+    }
+}
